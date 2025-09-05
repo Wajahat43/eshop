a26f63003c5a8f2b888e2938824dad75ccf350d8
Wajahat Islam Gul <aliwajahat44@yahoo.com>
2025-09-05 11:22:37 +0500

    fix: Fix product service not found error on render

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,27 +2,27 @@ module.exports = {
   apps: [
     {
       name: 'auth-service',
-      script: 'apps/auth-service/dist/main.js',
+      script: 'dist/apps/auth-service/main.js',
       env: { PORT: 6001 },
     },
     {
       name: 'product-service',
-      script: 'apps/product-service/dist/main.js',
+      script: 'dist/apps/product-service/main.js',
       env: { PORT: 6002 },
     },
     {
       name: 'order-service',
-      script: 'apps/order-service/dist/main.js',
+      script: 'dist/apps/order-service/main.js',
       env: { PORT: 6004 },
     },
     {
       name: 'chat-service',
-      script: 'apps/chat-service/dist/main.js',
+      script: 'dist/apps/chat-service/main.js',
       env: { PORT: 6005 },
     },
     {
       name: 'api-gateway',
-      script: 'apps/api-gateway/dist/main.js',
+      script: 'dist/apps/api-gateway/main.js',
       env: { PORT: 8080 },
     },
   ],
